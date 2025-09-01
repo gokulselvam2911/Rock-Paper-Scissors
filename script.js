aae8c5287576f2a32980b1ca0d75c442ef470f7b
gokulselvam2911 <gokulselvam2911@gmail.com>
2025-09-01 14:51:22 +0000

    Update index.html title and add pseudocode for game logic; implement getComputerChoice function in script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("Hello World");
+function getComputerChoice(){
+    let randomNum = Math.floor(Math.random()*3);
+
+    if (randomNum === 0){
+        return "ROCK";
+    } else if (randomNum === 1){
+        return "PAPER"
+    } else {
+        return "SCISSORS"
+    }
+}
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+console.log(getComputerChoice());
